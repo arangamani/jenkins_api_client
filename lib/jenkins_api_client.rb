af0de8e0ccc587a1e655004848d7ef37f52bd755
Kannan Manickam <kannan@rightscale.com>
2012-10-15 01:03:25 -0700

    removed require statements that are not needed

--- lib/jenkins_api_client.rb
+++ lib/jenkins_api_client.rb
@@ -1,10 +1,1 @@
-require 'rubygems'
-require 'json'
-require 'net/http'
-require 'base64'
-require 'nokogiri'
-require 'active_support/core_ext'
-require 'active_support/builder'
-require 'pp'
-
 require File.expand_path('../jenkins_api_client/client', __FILE__)
