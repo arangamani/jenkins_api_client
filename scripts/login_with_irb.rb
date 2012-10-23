c1806130c2a59844f0e3b89df0e8b06f9a876fa6
Kannan Manickam <kannan@rightscale.com>
2012-10-23 01:28:22 -0700

    added login_with_irb script

---
+++ scripts/login_with_irb.rb
@@ -0,0 +1,14 @@
+# This script provides an easier way to login to Jenkins server API.
+# It logs you in with the credentials and server details you proided and then starts an IRB
+# session so you can interactively play with the API.
+
+require File.expand_path('../../lib/jenkins_api_client', __FILE__)
+require 'yaml'
+require 'irb'
+
+begin
+  @client = JenkinsApi::Client.new(YAML.load_file(File.expand_path('~/.jenkins_api_client/login.yml', __FILE__)))
+  puts "logged-in to the Jenkins API, use the '@client' variable to use the client"
+end
+
+IRB.start
