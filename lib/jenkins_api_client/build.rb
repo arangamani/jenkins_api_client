3db7df31f2b9b24c1cbecd41430003341f5240d6
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-12-11 20:01:10 +0100

    Adds the ability to query data about a specific build.

---
+++ lib/jenkins_api_client/build.rb
@@ -0,0 +1,51 @@
+#
+# Copyright (c) 2012 Michael Pellon <michael@p3ll0n.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+module JenkinsApi
+  class Client
+    class Build
+
+class InvalidBuildAttribute < StandardError end
+
+      VALID_BUILD_ATTRS = [
+        "result"
+].freeze
+
+      def initialize(client)
+        @client = client
+      end
+
+      # Return a string representation of the object
+      #
+      def to_s
+        "#<JenkinsApi::Client::Build>"
+      end
+
+      def get_attr(job_number, build_number, attr)
+raise InvalidBuildAttribute if !VALID_BUILD_ATTRS.include?(attr)
+        response_json = @client.api_get_request("/job/#{@job_number}/#{@build_number}")
+        response_json[attr]
+      end
+
+end
+  end
+end
