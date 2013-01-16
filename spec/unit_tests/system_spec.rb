108ecbb0ea1c1811c5cf34c36e91c9fc51645b95
Kannan Manickam <arangamani.kannan@gmail.com>
2013-01-15 21:01:58 -0800

    updated system spec tests

--- spec/unit_tests/system_spec.rb
+++ spec/unit_tests/system_spec.rb
@@ -4,7 +4,7 @@ describe JenkinsApi::Client::System do
   context "With properly initialized Client" do
     before do
       @client = mock
-      @view = JenkinsApi::Client::System.new(@client)
+      @system = JenkinsApi::Client::System.new(@client)
     end
 
     describe "InstanceMethods" do
@@ -15,7 +15,40 @@ describe JenkinsApi::Client::System do
           ).not_to raise_error
         end
       end
-    end
 
+      describe "#quiet_down" do
+        it "sends a request to put the server in quiet down mode" do
+          @client.should_receive(:api_post_request).with("/quietDown")
+          @system.quiet_down
+        end
+      end
+
+      describe "#cancel_quiet_down" do
+        it "sends a request to take the server away from quiet down mode" do
+          @client.should_receive(:api_post_request).with("/cancelQuietDown")
+          @system.cancel_quiet_down
+        end
+      end
+
+      describe "#restart" do
+        it "sends a safe restart request to the server" do
+          @client.should_receive(:api_post_request).with("/safeRestart")
+          @system.restart(false)
+        end
+        it "sends a force restart request to the server" do
+          @client.should_receive(:api_post_request).with("/restart")
+          @system.restart(true)
+        end
+      end
+
+      describe "#wait_for_ready" do
+        it "exits if the response body doesn't have the wait message" do
+          @client.should_receive(:get_root).and_return(Net::HTTP.get_response(URI('http://example.com/index.html')))
+          @client.should_receive(:debug)
+          @system.wait_for_ready
+        end
+      end
+
+    end
   end
 end
