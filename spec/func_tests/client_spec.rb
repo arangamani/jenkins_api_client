e96c8c0a2ba201e5456a809dd8580ef024cc775a
Kannan Manickam <arangamani.kannan@gmail.com>
2013-01-19 02:03:00 -0800

    [Client] Refactored func tests

--- spec/func_tests/client_spec.rb
+++ spec/func_tests/client_spec.rb
@@ -7,45 +7,83 @@ require File.expand_path('../spec_helper', __FILE__)
 require 'yaml'
 
 describe JenkinsApi::Client do
-  context "Given valid credentials and server information in the ~/.jenkins_api_client/spec.yml" do
+  context "Given valid credentials and server information are given" do
     before(:all) do
       @creds_file = '~/.jenkins_api_client/spec.yml'
-      # Grabbing just the server IP in a variable so we can check for wrong credentials
-      @server_ip = YAML.load_file(File.expand_path(@creds_file, __FILE__))[:server_ip]
+      # Grabbing just the server IP in a variable so we can check
+      # for wrong credentials
+      @server_ip = YAML.load_file(
+        File.expand_path(@creds_file, __FILE__)
+      )[:server_ip]
       begin
-        @client = JenkinsApi::Client.new(YAML.load_file(File.expand_path(@creds_file, __FILE__)))
+        @client = JenkinsApi::Client.new(
+          YAML.load_file(File.expand_path(@creds_file, __FILE__))
+        )
       rescue Exception => e
         puts "WARNING: Credentials are not set properly."
         puts e.message
       end
     end
 
-    it "Should be able to toggle the debug value" do
-      value = @client.debug
-      @client.toggle_debug.should_not == value
-    end
+    describe "InstanceMethods" do
 
-    it "Should be able to initialize with valid credentials" do
-      client1 = JenkinsApi::Client.new(YAML.load_file(File.expand_path(@creds_file, __FILE__)))
-      client1.class.should == JenkinsApi::Client
-    end
-
-    it "Should fail if wrong credentials are given" do
-      begin
-        client2 = JenkinsApi::Client.new(:server_ip => @server_ip, :username => 'stranger', :password => 'hacked')
-        client2.job.list_all
-      rescue Exception => e
-        e.class.should == JenkinsApi::Exceptions::UnautherizedException
+      describe "#debug" do
+        it "Should be able to toggle the debug value" do
+          value = @client.debug
+          @client.toggle_debug.should_not == value
+        end
       end
-    end
 
-    it "Should return a job object on call to job function" do
-      @client.job.class.should == JenkinsApi::Client::Job
-    end
+      describe "#initialize" do
+        it "Should be able to initialize with valid credentials" do
+          client1 = JenkinsApi::Client.new(
+            YAML.load_file(File.expand_path(@creds_file, __FILE__))
+          )
+          client1.class.should == JenkinsApi::Client
+        end
 
-    it "Should accept a YAML argument when creating a new client" do
-      client3 = JenkinsApi::Client.new(YAML.load_file(File.expand_path(@creds_file, __FILE__)))
-      client3.class.should == JenkinsApi::Client
+        it "Should accept a YAML argument when creating a new client" do
+          client3 = JenkinsApi::Client.new(
+            YAML.load_file(File.expand_path(@creds_file, __FILE__))
+          )
+          client3.class.should == JenkinsApi::Client
+        end
+
+        it "Should fail if wrong credentials are given" do
+          begin
+            client2 = JenkinsApi::Client.new(:server_ip => @server_ip,
+                                             :username => 'stranger',
+                                             :password => 'hacked')
+            client2.job.list_all
+          rescue Exception => e
+            e.class.should == JenkinsApi::Exceptions::UnautherizedException
+          end
+        end
+      end
+
+      describe "#job" do
+        it "Should return a job object on call" do
+          @client.job.class.should == JenkinsApi::Client::Job
+        end
+      end
+
+      describe "#node" do
+        it "Should return a node object on call" do
+          @client.node.class.should == JenkinsApi::Client::Node
+        end
+      end
+
+      describe "#view" do
+        it "Should return a view object on call" do
+          @client.view.class.should == JenkinsApi::Client::View
+        end
+      end
+
+      describe "#system" do
+        it "Should return a system object on call" do
+          @client.system.class.should == JenkinsApi::Client::System
+        end
+      end
     end
 
   end
