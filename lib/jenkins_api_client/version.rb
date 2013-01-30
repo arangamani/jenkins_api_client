a9f44c8401df0b4b9c912d4d8334df07ec69d9ff
Kannan Manickam <arangamani.kannan@gmail.com>
2013-01-30 00:01:23 -0800

    bumped version to 0.7.1

--- lib/jenkins_api_client/version.rb
+++ lib/jenkins_api_client/version.rb
@@ -24,7 +24,7 @@ module JenkinsApi
   class Client
     MAJOR   = 0
     MINOR   = 7
-    TINY    = 0
+    TINY    = 1
     PRE     = nil
     VERSION = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
