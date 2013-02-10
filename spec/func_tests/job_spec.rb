7ca3a83754a08522f6cf5bdc7c2de90a15dd7dbd
Kannan Manickam <arangamani.kannan@gmail.com>
2013-02-10 01:47:04 -0800

    [Job] - Issue #35 - fixed func tests

--- spec/func_tests/job_spec.rb
+++ spec/func_tests/job_spec.rb
@@ -168,7 +168,7 @@ describe JenkinsApi::Client::Job do
             :timer => "* * * * *"
           }
           @client.job.create_freestyle(params).to_i.should == 200
-          @client.job.delete("test_job_using_timer").to_i.should == 200
+          @client.job.delete("test_job_using_timer").to_i.should == 302
         end
         it "Should accept child projects option" do
           params = {
