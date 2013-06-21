2d9073513affc363c5bb03f62ea211b78271519b
Lars Christensen <larsch@belunktum.dk>
2013-06-21 15:39:50 +0200

    Escape job names in URLs

--- lib/jenkins_api_client/job.rb
+++ lib/jenkins_api_client/job.rb
@@ -20,6 +20,8 @@
 # THE SOFTWARE.
 #
 
+require 'cgi'
+
 module JenkinsApi
   class Client
     # This class communicates with the Jenkins "/job" API to obtain details
@@ -45,7 +47,7 @@ module JenkinsApi
       # @param [XML] xml
       #
       def create(job_name, xml)
-        @client.post_config("/createItem?name=#{job_name}", xml)
+        @client.post_config("/createItem?name=#{CGI.escape job_name}", xml)
       end
 
       # Create a job with params given as a hash instead of the xml
@@ -275,7 +277,7 @@ module JenkinsApi
       # @param [String] new_job Name of the new job.
       #
       def rename(old_job, new_job)
-        @client.api_post_request("/job/#{old_job}/doRename?newName=#{new_job}")
+        @client.api_post_request("/job/#{CGI.escape old_job}/doRename?newName=#{CGI.escape new_job}")
       end
 
       # Delete a job given the name
@@ -283,7 +285,7 @@ module JenkinsApi
       # @param [String] job_name
       #
       def delete(job_name)
-        @client.api_post_request("/job/#{job_name}/doDelete")
+        @client.api_post_request("/job/#{CGI.escape job_name}/doDelete")
       end
 
       # Deletes all jobs from Jenkins
@@ -308,10 +310,10 @@ module JenkinsApi
         raise "No builds for #{job_name}" unless build_number
         # Check and see if the build is running
         is_building = @client.api_get_request(
-          "/job/#{job_name}/#{build_number}"
+          "/job/#{CGI.escape job_name}/#{build_number}"
         )["building"]
         if is_building
-          @client.api_post_request("/job/#{job_name}/#{build_number}/stop")
+          @client.api_post_request("/job/#{CGI.escape job_name}/#{build_number}/stop")
         end
       end
 
@@ -354,7 +356,7 @@ module JenkinsApi
         else
           raise "Mode should either be 'text' or 'html'. You gave: #{mode}"
         end
-        get_msg = "/job/#{job_name}/#{build_num}/logText/progressive#{mode}?"
+        get_msg = "/job/#{CGI.escape job_name}/#{build_num}/logText/progressive#{mode}?"
         get_msg << "start=#{start}"
         raw_response = true
         api_response = @client.api_get_request(get_msg, nil, nil, raw_response)
@@ -433,7 +435,7 @@ module JenkinsApi
       # @param [String] job_name
       #
       def list_details(job_name)
-        @client.api_get_request("/job/#{job_name}")
+        @client.api_get_request("/job/#{CGI.escape job_name}")
       end
 
       # List upstream projects of a specific job
@@ -441,7 +443,7 @@ module JenkinsApi
       # @param [String] job_name
       #
       def get_upstream_projects(job_name)
-        response_json = @client.api_get_request("/job/#{job_name}")
+        response_json = @client.api_get_request("/job/#{CGI.escape job_name}")
         response_json["upstreamProjects"]
       end
 
@@ -450,7 +452,7 @@ module JenkinsApi
       # @param [String] job_name
       #
       def get_downstream_projects(job_name)
-        response_json = @client.api_get_request("/job/#{job_name}")
+        response_json = @client.api_get_request("/job/#{CGI.escape job_name}")
         response_json["downstreamProjects"]
       end
 
@@ -459,7 +461,7 @@ module JenkinsApi
       # @param [String] job_name
       #
       def get_builds(job_name)
-        response_json = @client.api_get_request("/job/#{job_name}")
+        response_json = @client.api_get_request("/job/#{CGI.escape job_name}")
         response_json["builds"]
       end
 
@@ -500,7 +502,7 @@ module JenkinsApi
       # @return [String] status current status of the given job
       #
       def get_current_build_status(job_name)
-        response_json = @client.api_get_request("/job/#{job_name}")
+        response_json = @client.api_get_request("/job/#{CGI.escape job_name}")
         color_to_status(response_json["color"])
       end
 
@@ -512,7 +514,7 @@ module JenkinsApi
       # @return [Number] build_unumber current build number of the given job
       #
       def get_current_build_number(job_name)
-        @client.api_get_request("/job/#{job_name}")['nextBuildNumber'].to_i - 1
+        @client.api_get_request("/job/#{CGI.escape job_name}")['nextBuildNumber'].to_i - 1
       end
 
       # Build a job given the name of the job
@@ -525,9 +527,9 @@ module JenkinsApi
       #
       def build(job_name, params={})
         if params.empty?
-          @client.api_post_request("/job/#{job_name}/build")
+          @client.api_post_request("/job/#{CGI.escape job_name}/build")
         else
-          @client.api_post_request("/job/#{job_name}/buildWithParameters", params)
+          @client.api_post_request("/job/#{CGI.escape job_name}/buildWithParameters", params)
         end
       end
 
@@ -538,7 +540,7 @@ module JenkinsApi
       # @return [String] XML Config.xml of the job
       #
       def get_config(job_name)
-        @client.get_config("/job/#{job_name}")
+        @client.get_config("/job/#{CGI.escape job_name}")
       end
 
       # Post the configuration of a job given the job name and the config.xml
@@ -549,7 +551,7 @@ module JenkinsApi
       # @return [String] response_code return code from HTTP POST
       #
       def post_config(job_name, xml)
-        @client.post_config("/job/#{job_name}/config.xml", xml)
+        @client.post_config("/job/#{CGI.escape job_name}/config.xml", xml)
       end
 
       # Obtain the test results for a specific build of a job
@@ -560,7 +562,7 @@ module JenkinsApi
       def get_test_results(job_name, build_num)
         build_num = get_current_build_number(job_name) if build_num == 0
 
-        @client.api_get_request("/job/#{job_name}/#{build_num}/testReport")
+        @client.api_get_request("/job/#{CGI.escape job_name}/#{build_num}/testReport")
       rescue Exceptions::NotFoundException
         # Not found is acceptable, as not all builds will have test results
         # and this is what jenkins throws at us in that case
@@ -575,7 +577,7 @@ module JenkinsApi
       def get_build_details(job_name, build_num)
         build_num = get_current_build_number(job_name) if build_num == 0
 
-        @client.api_get_request("/job/#{job_name}/#{build_num}/")
+        @client.api_get_request("/job/#{CGI.escape job_name}/#{build_num}/")
       end
 
       # Change the description of a specific job
