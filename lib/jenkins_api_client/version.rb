6776d219a78ba904e50d2c3632299f09a8a59212
Kannan Manickam <arangamani.kannan@gmail.com>
2013-06-28 03:41:12 -0700

    bump version for development iteration

--- lib/jenkins_api_client/version.rb
+++ lib/jenkins_api_client/version.rb
@@ -25,11 +25,11 @@ module JenkinsApi
     # Major version of the gem
     MAJOR   = 0
     # Minor version of the gem
-    MINOR   = 12
+    MINOR   = 13
     # Tiny version of the gem used for patches
-    TINY    = 1
+    TINY    = 0
     # Used for pre-releases
-    PRE     = nil
+    PRE     = "pre1"
     # Version String of Jenkins API Client.
     VERSION = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
