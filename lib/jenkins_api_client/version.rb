68cf33ce7a5217e4312ac3465e38b413b1300c1d
Elliott Murray <emurray@dius.com.au>
2013-10-15 10:46:56 +1100

    Updated version

--- lib/jenkins_api_client/version.rb
+++ lib/jenkins_api_client/version.rb
@@ -27,7 +27,7 @@ module JenkinsApi
     # Minor version of the gem
     MINOR   = 15
     # Tiny version of the gem used for patches
-    TINY    = 2
+    TINY    = 3
     # Used for pre-releases
     PRE     = nil
     # Version String of Jenkins API Client.
