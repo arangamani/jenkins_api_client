7eb2ec0cbe360e0720f1fea159759dfded9a78c2
Spike Grobstein <sgrobstein@shutterstock.com>
2014-01-15 12:26:59 -0500

    added some comments to document

--- lib/jenkins_api_client/client.rb
+++ lib/jenkins_api_client/client.rb
@@ -69,7 +69,7 @@ module JenkinsApi
     #
     # @option args [String] :server_ip the IP address of the Jenkins CI server
     # @option args [String] :server_port the port on which the Jenkins listens
-    # @option args [String] :server_url the full URL address of the Jenkins CI server (http/https)
+    # @option args [String] :server_url the full URL address of the Jenkins CI server (http/https). This can include username/password. :username/:password options will override any user/pass value in the URL
     # @option args [String] :username the username used for connecting to the server (optional)
     # @option args [String] :password the password or API Key for connecting to the CI server (optional)
     # @option args [String] :password_base64 the password with base64 encoded format for connecting to the CI
@@ -112,6 +112,9 @@ module JenkinsApi
         @server_port = server_uri.port
         @ssl = server_uri.scheme == "https"
         @jenkins_path = server_uri.path
+
+        # read username and password from the URL
+        # only set if @username and @password are not already set via explicit options
         @username ||= server_uri.user
         @password ||= server_uri.password
       end
